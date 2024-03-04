a8b19ad12863564eab955afa3d2a6468df63580c
tanmana183 <dastanmana2002@gmail.com>
2024-03-04 20:29:32 +0530

    final update quiz app

--- quizapp/app.js
+++ quizapp/app.js
@@ -1,23 +1,33 @@
-const questionbox=document.getElementById("questionbox")
-const answ=document.getElementById("answer")
-const next=document.getElementById("next")
-const questions=[
+const questionbox = document.getElementById("questionbox")
+const answer = document.getElementById("answer")
+const next = document.getElementById("next")
+const quiz=document.getElementsByClassName("quizbox")
+const questions = [
     {
-        question:"What is the name of India's national bird?",
-        answer:[
-            {text:"owl",correct:false},
-            {text:"peacock",correct:true},
-            {text:"crow",correct:false},
-            {text:"sparrow",correct:false}
+        question: "What is the name of India's national bird?",
+        answer: [
+            { text: "owl", correct: false },
+            { text: "peacock", correct: true },
+            { text: "crow", correct: false },
+            { text: "sparrow", correct: false }
         ]
     },
     {
-        question:"What is the name of India's national fruit?",
-        answer:[
-            {text:"pineapple",correct:false},
-            {text:"mango",correct:true},
-            {text:"litchie",correct:false},
-            {text:"guava",correct:false}
+        question: "What is the name of India's national fruit?",
+        answer: [
+            { text: "pineapple", correct: false },
+            { text: "mango", correct: true },
+            { text: "litchie", correct: false },
+            { text: "guava", correct: false }
+        ]
+    },
+    {
+        question: "What is the name of India's national animal?",
+        answer: [
+            { text: "gorilla", correct: false },
+            { text: "monkey", correct: true },
+            { text: "donkey", correct: false },
+            { text: "cow", correct: false }
         ]
     },
 ]
@@ -25,29 +35,109 @@ const questions=[
 let index = 0
 let score = 0
 function start() {
-
+    index = 0
+    score = 0
     next.innerHTML = "Next"
     showquestion()
 }
 function showquestion() {
 
+    resetstate()
+
     prev = questions[index]
-    currentquestion = prev.question
+
+
+
     qno = index + 1
+
+    questionbox.innerHTML = qno + ". " + questions[index].question;
+
     // console.log(prev.answer)
-    questionbox.innerHTML = qno + ". " + currentquestion;
-    prev.answer.forEach(element => {
-        console.log(element.text)
+    questions[index].answer.forEach(element => {
+
         const button = document.createElement("button")
         button.innerHTML = element.text
         button.classList.add("ans")
-        answ.appendChild(button)
-        button.addEventListener("click",selectans)
+        answer.appendChild(button)
+
+        if (element.correct) {
+
+            button.dataset.correct = element.correct
+
+        }
+        button.addEventListener("click", selectans)
     });
-}
-function selectans(e){
     
 }
+function resetstate() {
+    next.style.display = "none"
+    while (answer.firstChild) {
+        answer.removeChild(answer.firstChild)
+    }
+}
+function selectans(e) {
+    const selectbtn = e.target
+
+    const iscorrect = selectbtn.dataset.correct === "true"
+    if (iscorrect) {
+        selectbtn.classList.add("correct")
+        score++;
+    }
+    else {
+        selectbtn.classList.add("incorrect")
+    }
+    Array.from(answer.children).forEach(button => {
+
+        if (button.dataset.correct === "true") {
+
+            button.classList.add("correct")
+        }
+        button.disabled = true
+    })
+    next.style.display = "block"
+}
+function scoreshow() {
+
+    resetstate();
+    
+    
+    questionbox.innerHTML = `You scored ${score} out of ${questions.length}`
+    
+   
+    next.innerHTML = "Play Again"
+    next.style.display = "block"
+
+    console.log("k")
+    
+    index = -1
+    
+}
+
+function handlenext() {
+    if (index < questions.length - 1) {
+
+        index++;
+
+
+        showquestion()
+
+
+    }
+    else {
+
+        scoreshow()
+    }
+}
+next.addEventListener("click", () => {
+    next.innerHTML = "Next"
+    if (index < questions.length) {
+        handlenext();
+
+    }
+    else {
+        start()
+    }
+})
 start()
 
 
